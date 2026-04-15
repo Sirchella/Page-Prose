8747144c337251f67ab1a3d13466ebb8f52aa3c6
AJA CHELLA ASAMBA JR <ajachella.asamba@ictuniversity.edu.cm>
2026-04-15 22:33:54 +0100

    Fix start.sh: use exec gunicorn, --run-syncdb, add workers and timeout

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ set -e
 
 cd backend
 pip install -r requirements.txt
-python manage.py migrate --no-input
+python manage.py migrate --run-syncdb --no-input
 python manage.py collectstatic --no-input
-gunicorn page_and_prose.wsgi --bind 0.0.0.0:${PORT:-8000}
+exec gunicorn page_and_prose.wsgi --bind 0.0.0.0:${PORT:-8000} --workers 2 --timeout 120
